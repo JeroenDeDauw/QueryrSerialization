69812de7433e1ce0d39cd501a5acb19672a5881e
jeroendedauw <jeroendedauw@gmail.com>
2014-05-20 16:27:32 +0200

    Implement first version of SimpleItemSerializer

--- tests/unit/SimpleItemSerializerTest.php
+++ tests/unit/SimpleItemSerializerTest.php
@@ -2,8 +2,17 @@
 
 namespace Tests\Queryr\Serialization\DataModel;
 
+use DataValues\StringValue;
 use Queryr\Serialization\DataModel\SimpleItemSerializer;
-use Serializers\Serializer;
+use Wikibase\DataModel\Claim\Statement;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\DataModel\SiteLinkList;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Snak\PropertyValueSnak;
+use Wikibase\DataModel\Term\AliasGroup;
+use Wikibase\DataModel\Term\Fingerprint;
+use Wikibase\DataModel\Term\Term;
 
 /**
  * @covers Queryr\Serialization\DataModel\SimpleItemSerializer
@@ -13,18 +22,154 @@ use Serializers\Serializer;
  */
 class SimpleItemSerializerTest extends \PHPUnit_Framework_TestCase {
 
-	/**
-	 * @var Serializer
-	 */
-	private $serializer;
+	public function testGivenNonItem_exceptionIsThrown() {
+		$serializer = new SimpleItemSerializer();
 
-	public function setUp() {
-		$this->serializer = new SimpleItemSerializer();
+		$this->setExpectedException( 'Serializers\Exceptions\UnsupportedObjectException' );
+		$serializer->serialize( null );
 	}
 
-	public function testGivenNonItem_exceptionIsThrown() {
-		$this->setExpectedException( 'Serializers\Exceptions\UnsupportedObjectException' );
-		$this->serializer->serialize( null );
+	private function newItem() {
+		$item = Item::newEmpty();
+
+		$item->setId( 1337 );
+
+		$item->setFingerprint( $this->newFingerprint() );
+		$item->setSiteLinkList( $this->newSiteLinks() );
+
+		$this->addClaims( $item );
+
+		return $item;
+	}
+
+	private function newFingerprint() {
+		$fingerprint = Fingerprint::newEmpty();
+
+		$fingerprint->setLabel( new Term( 'en', 'foo' ) );
+		$fingerprint->setLabel( new Term( 'de', 'bar' ) );
+		$fingerprint->setLabel( new Term( 'nl', 'baz' ) );
+
+		$fingerprint->setDescription( new Term( 'de', 'de description' ) );
+
+		$fingerprint->setAliasGroup( new AliasGroup( 'en', [ 'first en alias', 'second en alias' ] ) );
+		$fingerprint->setAliasGroup( new AliasGroup( 'de', [ 'first de alias', 'second de alias' ] ) );
+
+		return $fingerprint;
+	}
+
+	private function newSiteLinks() {
+		$links = new SiteLinkList();
+
+		$links->add( new SiteLink( 'enwiki', 'En Page' ) );
+		$links->add( new SiteLink( 'dewiki', 'De Page' ) );
+
+		return $links;
+	}
+
+	private function addClaims( Item $item ) {
+		$claim = new Statement( new PropertyValueSnak( 42, new StringValue( 'kittens' ) ) );
+		$claim->setGuid( 'first guid' );
+
+		$item->addClaim( $claim );
+
+		$claim = new Statement( new PropertyNoValueSnak( 23 ) );
+		$claim->setGuid( 'second guid' );
+
+		$item->addClaim( $claim );
+	}
+
+	public function testSerializationForDe() {
+		$serializer = new SimpleItemSerializer( 'de' );
+
+		$item = $this->newItem();
+
+		$serialized = $serializer->serialize( $item );
+
+		$expected = [
+			'id' => [
+				'wikidata' => 'Q1337',
+				'en.wikipedia' => 'En Page',
+				'de.wikipedia' => 'De Page',
+			],
+
+			'label' => 'bar',
+			'description' => 'de description',
+			'aliases' => [ 'first de alias', 'second de alias' ],
+
+			'data' => [
+				'P42' => [
+					'value' => 'kittens',
+					'type' => 'string'
+				],
+
+				'P23' => [
+					'type' => 'novalue'
+				],
+			]
+		];
+
+		$this->assertEquals( $expected, $serialized );
+	}
+
+	public function testSerializationForEn() {
+		$serializer = new SimpleItemSerializer( 'en' );
+
+		$item = $this->newItem();
+
+		$serialized = $serializer->serialize( $item );
+
+		$expected = [
+			'id' => [
+				'wikidata' => 'Q1337',
+				'en.wikipedia' => 'En Page',
+			],
+
+			'label' => 'foo',
+			'aliases' => [ 'first en alias', 'second en alias' ],
+
+			'data' => [
+				'P42' => [
+					'value' => 'kittens',
+					'type' => 'string'
+				],
+
+				'P23' => [
+					'type' => 'novalue'
+				],
+			]
+		];
+
+		$this->assertEquals( $expected, $serialized );
+	}
+
+	public function testSerializationForNl() {
+		$serializer = new SimpleItemSerializer( 'nl' );
+
+		$item = $this->newItem();
+
+		$serialized = $serializer->serialize( $item );
+
+		$expected = [
+			'id' => [
+				'wikidata' => 'Q1337',
+				'en.wikipedia' => 'En Page',
+			],
+
+			'label' => 'baz',
+
+			'data' => [
+				'P42' => [
+					'value' => 'kittens',
+					'type' => 'string'
+				],
+
+				'P23' => [
+					'type' => 'novalue'
+				],
+			]
+		];
+
+		$this->assertEquals( $expected, $serialized );
 	}
 
 }
