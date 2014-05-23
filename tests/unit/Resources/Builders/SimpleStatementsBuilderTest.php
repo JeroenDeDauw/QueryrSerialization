4e1cf18c0d35a2f553a0f9e2b4d2f7f216641129
jeroendedauw <jeroendedauw@gmail.com>
2014-05-23 18:44:29 +0200

    Clean up test

--- tests/unit/Resources/Builders/SimpleStatementsBuilderTest.php
+++ tests/unit/Resources/Builders/SimpleStatementsBuilderTest.php
@@ -3,20 +3,11 @@
 namespace Tests\Queryr\Resources\Builders;
 
 use DataValues\StringValue;
-use Queryr\Resources\Builders\SimpleItemBuilder;
 use Queryr\Resources\Builders\SimpleStatementsBuilder;
-use Queryr\Resources\SimpleItem;
 use Queryr\Resources\SimpleStatement;
 use Wikibase\DataModel\Claim\ClaimList;
 use Wikibase\DataModel\Claim\Statement;
-use Wikibase\DataModel\Entity\Item;
-use Wikibase\DataModel\SiteLink;
-use Wikibase\DataModel\SiteLinkList;
-use Wikibase\DataModel\Snak\PropertyNoValueSnak;
 use Wikibase\DataModel\Snak\PropertyValueSnak;
-use Wikibase\DataModel\Term\AliasGroup;
-use Wikibase\DataModel\Term\Fingerprint;
-use Wikibase\DataModel\Term\Term;
 
 /**
  * @covers Queryr\Resources\Builders\SimpleStatementsBuilder
@@ -26,21 +17,7 @@ use Wikibase\DataModel\Term\Term;
  */
 class SimpleStatementsBuilderTest extends \PHPUnit_Framework_TestCase {
 
-
-
-	private function addClaims( Item $item ) {
-		$claim = new Statement( new PropertyValueSnak( 42, new StringValue( 'kittens' ) ) );
-		$claim->setGuid( 'first guid' );
-
-		$item->addClaim( $claim );
-
-		$claim = new Statement( new PropertyNoValueSnak( 23 ) );
-		$claim->setGuid( 'second guid' );
-
-		$item->addClaim( $claim );
-	}
-
-	public function testSerializationForDe() {
+	public function testBuildFromSingleStatementWithPropertyValueSnak() {
 		$statement = new Statement( new PropertyValueSnak( 42, new StringValue( 'kittens' ) ) );
 		$statement->setGuid( 'first guid' );
 
